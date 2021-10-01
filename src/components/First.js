91e8675256d713289b357ee873eee77355a0efaa
gajentiran <apgajentiran96@gmail.com>
2021-10-01 23:36:43 +0530

    first commiting

---
+++ src/components/First.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class First extends React.Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return <div>
+            <h1>here</h1>
+        </div>
+    }
+}
